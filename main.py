f34ce030fd47eb0095cbc7cde4228a19cf2b41fb
Grant Biellak <gbiellak@gmail.com>
2025-07-06 04:29:12 -0700

    Starting to build through main

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ import sys
 paddleocr_path = os.path.join(os.path.dirname(__file__), "PaddleOCR")
 sys.path.append(paddleocr_path)
 from image_recognition import trueOCR
+performInitial()
+time.sleep(1)
 regions = find_area()
 SCREENSHOT_AREA1, SCREENSHOT_AREA2, SCREENSHOT_AREA3, SCREENSHOT_AREA4 = regions
-
-counter = 0
-'''for i in range(1):
-    time.sleep(1.5)
+time.sleep(2)
+for i in range(5):
+    if i == 0:
+        firstofday()
+    time.sleep(3)
+    reroll()
+    time.sleep(2)
     trueOCR(SCREENSHOT_AREA1)
     trueOCR(SCREENSHOT_AREA2)
     trueOCR(SCREENSHOT_AREA3)
     trueOCR(SCREENSHOT_AREA4)
-    time.sleep(0.5)
-    counter += 1'''
 
